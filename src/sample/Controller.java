0eeca75d78ea5ccef1d6e9e94ddd464c9056e827
Nikita Vetyugov <nickitavet1998@gmail.com>
2021-04-20 21:21:39 +0300

    Тяжело разбираться с файлом sample...

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,10 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-
-import java.awt.event.KeyEvent;
-import java.util.*;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-import javafx.scene.control.*;
-
-//import java.awt.event.KeyEvent;
 
 public class Controller {
     @FXML
